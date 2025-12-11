945a992b09c65845ada2502a0b59f089c09e6e09
Abhishek Yadav <abhiiyadav933640@gmail.com>
2025-12-11 15:58:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+// showing navbar when click menu on mobile view
+
+const mobile = document.querySelector('.menu-toggle');
+const mobilelink = document.querySelector('.sidebar');
+
+mobile.addEventListener("click", function(){
+    mobile.classList.toggle("is-active");
+    mobilelink.classList.toggle("active");
+})
+
+// close menu on click
+
+mobilelink.addEventListener("click", function(){
+    const menuBars = document.querySelector(".is-active");
+    if(window.innerWidth<=768 && menuBars){
+        mobile.classList.toggle("is-active");
+        mobilelink.classList.toggle("active");
+    }
+})
+
+// move the menu to right and left when click back and next 
+
+var step = 100;
+var stepFilter = 60;
+var scrolling = true;
+
+$(".back").bind("click", function(e){
+    e.preventDefault();
+    $(".heighlight-wrapper").animate({
+        scrollLeft: "-=" + step + "px"
+    })
+})
+
+$(".next").bind("click", function(e){
+    e.preventDefault();
+    $(".heighlight-wrapper").animate({
+        scrollLeft: "+=" + step + "px"
+    })
+})
+
+// when click on back and next on menu filters
+
+$(".back-menus").bind("click", function(e){
+    e.preventDefault();
+    $(".heighlight-wrapper").animate({
+        scrollLeft: "-=" + stepFilter + "px"
+    })
+})
+
+$(".next-menus").bind("click", function(e){
+    e.preventDefault();
+    $(".heighlight-wrapper").animate({
+        scrollLeft: "-=" + stepFilter + "px"
+    })
+})
